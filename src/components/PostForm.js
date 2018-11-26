d1ac67eb4fdc3c4efbacfb81fb0154b29965d2b8
Shim Soyoung <sovv12@gmail.com>
2018-11-26 14:08:19 +0900

    PostForm CSS 모듈 작성

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import s from './PostForm.module.scss'
 
 // defaultValue 에 다른 값을 또 넣어주지않도록 주의. 한번 값을 넣어줬으면 그 값이 유지되게 한다.
 export default class PostForm extends Component {
@@ -6,9 +7,9 @@ export default class PostForm extends Component {
     return (
       <div>
         <form onSubmit={e => this.props.onSubmit(e)}>
-          <input type="text" name="title" defaultValue={this.props.title} />
+          <input className={s['title-input']} type="text" name="title" defaultValue={this.props.title} />
           <textarea name="body" cols="30" rows="10" defaultValue={this.props.body}></textarea>
-          <button>작성</button>
+          <button className={s.formButton}>작성</button>
         </form>
       </div>
     )
